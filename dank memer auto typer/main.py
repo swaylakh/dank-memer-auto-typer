808bc5949396748172427b3ca22d05492b7e4511
Swayam Lakhotia <72654570+Swayx113@users.noreply.github.com>
2020-10-12 21:51:09 +0530

    Add files via upload

---
+++ dank memer auto typer/main.py
@@ -0,0 +1,26 @@
+import pynput
+import time
+from pynput.keyboard import Key, Controller
+
+keyboard = Controller()
+i = 0
+
+while i < 1:
+    
+    keyboard.type('pls hunt')
+    keyboard.press(Key.enter)
+    time.sleep(2)
+    keyboard.type('pls fish')
+    keyboard.press(Key.enter)
+    time.sleep(3)
+    keyboard.type('pls pm')
+    keyboard.press(Key.enter)
+    time.sleep(2)
+    keyboard.type('d')
+    keyboard.press(Key.enter)
+    time.sleep(2)
+    keyboard.type('pls beg')
+    keyboard.press(Key.enter)
+    time.sleep(2)
+    time.sleep(50)
+
